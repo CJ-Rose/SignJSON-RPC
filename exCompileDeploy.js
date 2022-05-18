274104a526029e634228b5223aac99236f35c21d
Colleen Rose <cjrose59@gmail.com>
2022-05-17 19:20:10 -0700

    add example deployment

---
+++ exCompileDeploy.js
@@ -0,0 +1,42 @@
+const ethers = require('ethers');
+const solc = require('solc');
+const ganache = require("ganache-core");
+
+const randomWallet = ethers.Wallet.createRandom();
+const ganacheProvider = ganache.provider({
+    accounts: [{
+        balance: ethers.utils.parseEther("10").toString(),
+        secretKey: randomWallet.privateKey,
+    }]
+});
+
+const provider = new ethers.providers.Web3Provider(ganacheProvider);
+const wallet = randomWallet.connect(provider);
+
+async function deploy() {
+    const content = `
+        pragma solidity ^0.8.11;
+        contract Contract {
+            uint public x;
+            constructor(uint _x) {
+                x = _x;
+            }
+        }
+    `;
+
+    const input = {
+        language: 'Solidity',
+        sources: { 'contract.sol': { content } },
+        settings: { outputSelection: { '*': { '*': ['*'] } } }
+    };
+    const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+    const { Contract } = output.contracts['contract.sol'];
+    const factory = ethers.ContractFactory.fromSolidity(Contract, wallet);
+    const contract = await factory.deploy(5);
+
+    const x = await contract.x();
+    console.log(x.toString());
+}
+
+deploy();
